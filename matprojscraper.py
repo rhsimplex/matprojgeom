b468baba1f1bbfda5d57c3b8d8a45b91f6b99a6d
rhsimplex <hendersonry@gmail.com>
2014-08-05 19:01:43 -0700

    added comments

--- matprojscraper.py
+++ matprojscraper.py
@@ -1,18 +1,35 @@
 import pymatgen as pm
 import os
 
-def scraper(dest_folder,  API_key='3QUoyXBlHChz5OLg', resume=True, verbose=False):
+def scraper(dest_folder,  API_key, resume=0, verbose=False):
+    """
+    Helper method for scraping database entries from the Materials Project database, in order of Materials Project ID.  Automatically resumes from largest ID in the destination folder.
+    
+    Keyword arguments:
+    dest_folder -- path to store structure files
+    API_key -- Materials project API key
+    resume -- Materials Project ID to resume scraping from. I.e. 1234 will start with ID 'mp-1234', unless the dest_folder contains a file 'mp-XXXX' where XXXX is an integer > 1234.
+    verbose -- print messages
+    """
     last_materialsproject_id = 1
+    
+    # Create folder if none exists
     if not os.path.exists(dest_folder):
         if verbose: print 'No folder, creating ' + dest_folder
         os.mkdir(dest_folder)
     files_list = os.listdir(dest_folder)
-    if len(files_list) > 0 and resume:
+    
+    # Find last file by Materials Project ID
+    if len(files_list) > 0 and resume > 0:
         ids = map(lambda x: int(x.replace('mp-','').replace('.mson','')), files_list)
         last_materialsproject_id = sorted(ids)[-1]
-
+        if resume > last_materialsproject_id:
+            last_materialsproject_id = resume
+    
+    #Creat MPRester object
     m = pm.MPRester(API_key)
-
+    
+    #Loop indefinitely querying database for entries, in order.  If entry doesn't exist, move to next ID. Structure objects saved in Materials Project '.mson' format
     while True:
         try:
             mpname = 'mp-' + str(last_materialsproject_id)
