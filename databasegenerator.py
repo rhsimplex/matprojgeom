5905b77fdcb0096bb1b0ca988a30bbf101374167
rhsimplex <hendersonry@gmail.com>
2014-08-06 05:56:09 -0700

    more functions

--- databasegenerator.py
+++ databasegenerator.py
@@ -2,6 +2,7 @@ import numpy as np
 import pymatgen as pm
 import pandas as pd
 import os
+from operator import mul
 
 def csvbuilder(target_dir, output, column_functions):
     """
@@ -29,8 +30,53 @@ def csvbuilder(target_dir, output, column_functions):
     df.to_csv(output)
     return df
 
+#list of column functions you might want. Of course you can use any function that accepts a Structure object and returns a single value
 def formula(a):
     return a.formula
 
 def numberOfSpecies(a):
     return len(a.composition)
+
+def fracTransitionMetal(a):
+    c = a.composition
+    return sum(map(mul, [x.is_transition_metal for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracRareEarth(a):
+    c = a.composition
+    return sum(map(mul, [x.is_rare_earth_metal for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracNobleGas(a):
+    c = a.composition
+    return sum(map(mul, [x.is_noble_gas for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracMetalloid(a):
+    c = a.composition
+    return sum(map(mul, [x.is_metalloid for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracLanthanoid(a):
+    c = a.composition
+    return sum(map(mul, [x.is_lanthanoid for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracHalogen(a):
+    c = a.composition
+    return sum(map(mul, [x.is_halogen for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracChalcogen(a):
+    c = a.composition
+    return sum(map(mul, [x.is_chalcogen for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracAlkaline(a):
+    c = a.composition
+    return sum(map(mul, [x.is_alkaline for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracAlkali(a):
+    c = a.composition
+    return sum(map(mul, [x.is_alkali for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def fracActinoid(a):
+    c = a.composition
+    return sum(map(mul, [x.is_actinoid for x in c.elements], map(c.get_atomic_fraction, c.elements)))
+
+def electronsPerAtom(a):
+    c = a.composition
+    return sum(map(mul, map(c.get_atomic_fraction, c.elements),[x.common_oxidation_states[0] for x in c.elements]))
